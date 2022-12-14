c4da2f011ffa46d62dabd06f4c0d7cde97e76953
Daniil <daniil.sharin667@gmail.com>
2022-12-14 14:36:22 +0200

    if this works I will kill myself.

--- src/pages/api/updateDatabase.ts
+++ src/pages/api/updateDatabase.ts
@@ -3,10 +3,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Database } from "../../server/middleware/Database";
 
 async function updateDatabase(req: NextApiRequest, res: NextApiResponse) {
-    // await Database.updateBarboraItems();
-    // await Database.updateRimiItems();
-    // await Database.updateCoopItems();
-    // await Database.createStatsForEverything();
+    await Database.updateBarboraItems();
+    await Database.updateRimiItems();
+    await Database.updateCoopItems();
+    await Database.createStatsForEverything();
     
     return res.status(200).json({});
 }
