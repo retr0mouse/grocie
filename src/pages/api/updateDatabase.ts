59eaffb9f3eaa3c2a135d5468e325a884b8fbc57
Daniil <daniil.sharin667@gmail.com>
2022-12-14 14:52:13 +0200

    let's try this

--- src/pages/api/updateDatabase.ts
+++ src/pages/api/updateDatabase.ts
@@ -1,13 +1,19 @@
 import { verifySignature } from "@upstash/qstash/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { resourceLimits } from "worker_threads";
 import { Database } from "../../server/middleware/Database";
 
 async function updateDatabase(req: NextApiRequest, res: NextApiResponse) {
-    // await ;
-    // await Database.updateRimiItems();
-    // await Database.updateCoopItems();
-    // await Database.createStatsForEverything();
-    Promise.allSettled([Database.updateBarboraItems()]);
+    try {
+        Promise.all([
+            await Database.updateBarboraItems(), 
+            // await Database.updateRimiItems(),
+            // await Database.updateCoopItems(),
+            // await Database.createStatsForEverything()
+        ]);
+    } catch (error) {
+        console.log("error: " + error);
+    }
     return res.status(200).json({});
 }
 
