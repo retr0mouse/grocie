1c7da6d216a15b1f13b083a935e8d03d6dee5256
Daniil <daniil.sharin667@gmail.com>
2022-12-14 10:58:58 +0200

    cron authorization within a function fix

--- src/pages/api/updateDatabase.ts
+++ src/pages/api/updateDatabase.ts
@@ -1,14 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { env } from "process";
-import { Database } from "../../server/middleware/Database";
 import { verifySignature } from "@upstash/qstash/nextjs";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Database } from "../../server/middleware/Database";
 
 async function updateDatabase(req: NextApiRequest, res: NextApiResponse) {
-    const token = req.headers.authorization;
-    if (token !== env.CRON_TOKEN) {
-        res.status(401).json({ error: 'Unauthorized'});
-        return;
-    }
 
     Promise.allSettled([
         await Database.updateBarboraItems(),
