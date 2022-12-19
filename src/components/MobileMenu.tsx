6a99398fc3341dc8fc8a54070d02330fe0c75bdc
Daniil <daniil.sharin667@gmail.com>
2022-12-20 00:20:45 +0200

    mobile menu style fix

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -16,6 +16,7 @@ import MilkPicture from '../images/milk.svg';
 import OtherPicture from '../images/other.svg';
 import BrushPicture from '../images/toothbrush.svg';
 import VegetablesPicture from '../images/vegetables.svg';
+import CrossPicture from '../images/cross.svg';
 
 const categories = [
     [VegetablesPicture, "Köögiviljad, puuviljad"],
@@ -59,9 +60,14 @@ export default function MobileMenu() {
                     {/* Container to center the panel */}
                     <div className="flex min-h-full items-center justify-center p-4 w-full h-full">
                         {/* The actual dialog panel  */}
-                        <Dialog.Panel className="p-5 mx-auto max-w-full max-h-full rounded bg-white w-full h-full">
-                            <Dialog.Title className='text-3xl'>Kategooriad</Dialog.Title>
-                            <div className=" bg-white flex flex-col place-content-center">
+                        <Dialog.Panel className="rounded-sm mx-auto max-w-full max-h-full bg-white w-full h-full">
+                            <div className="p-5 flex rounded-t-sm bg-neutral-200 w-full border-b-2 items-center justify-between">
+                                <Dialog.Title className='text-3xl font-normal leading-normal mt-0 mb-2 text-orange-800'>Kategooriad</Dialog.Title>
+                                <button className="w-8 h-8" onClick={() => setIsOpen(false)}>
+                                    <Image className='w-full h-full' src={CrossPicture} alt='arrow down image'></Image>
+                                </button>
+                            </div>
+                            <div className="p-5 bg-white flex flex-col place-content-center">
                                 {categories.map((category, index) => {
                                     return (
                                         <Link href={`/category/${index}`} onClick={() => setIsOpen(false)}>
