93fbba6926097c0a24505b0cbe7c425d86d956b2
Daniil <daniil.sharin667@gmail.com>
2023-01-04 10:39:01 +0200

    trying to fix the mobile issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/pages/**/*.{js,ts,jsx,tsx}",
-  "./src/components/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  fontFamily: {
-    'sans': ['ui-sans-serif', 'system-ui'],
-    'serif': ['ui-serif', 'Georgia'],
-    'mono': ['ui-monospace', 'SFMono-Regular'],
-    'display': ['Oswald'],
-    'body': ['"Open Sans"'],
-  }
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    }
+  },
+  plugins: [],
+}
